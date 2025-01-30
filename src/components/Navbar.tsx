dbebe399f71ec1856b0770758f599991d292caa6
Diego França <diego.franca92@gmail.com>
2025-01-30 14:16:40 -0300

    complement repo link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,15 @@
 "use client"
 import { useAuth } from "@/hooks";
 import Link from "next/link";
+import { GithubIcon } from "./icons";
 
 export default function Navbar() {
   const { isAuthenticated, logout } = useAuth();
 
   return (
     <nav className="bg-gray-800 text-white p-4">
-      <div className="container mx-auto flex justify-center items-center">
+      <div className="container mx-auto flex justify-between items-center w-full">
+        <Link href="https://github.com/diegofranca92/notification-project-test" className="flex gap-4"><GithubIcon /> Repo Projeto</Link>
         <div className="space-x-4">
           {isAuthenticated && (
             <>
